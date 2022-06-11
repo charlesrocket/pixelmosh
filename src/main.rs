428831a65d1c2180e8efd83b700f29d4e4139ce4
-k <slowdive@me.com>
2022-06-10 17:59:49 -0700

    Start with PNGs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::fs::File;
+use rand::thread_rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = thread_rng();
+    let decoder = png::Decoder::new(File::open("test.png").unwrap());
+    let mut reader = decoder.read_info().unwrap();
+    let mut buf = vec![0; reader.output_buffer_size()];
+    let info = reader.next_frame(&mut buf).unwrap();
+    let bytes = &buf[..info.buffer_size()];
+    dbg!(bytes);
 }
