1fdcd4ca85a40f6e5e5d7077be4556a74862c18e
-k <slowdive@me.com>
2022-06-18 21:11:01 -0700

    Add cli tests

---
+++ tests/cli.rs
@@ -0,0 +1,34 @@
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use std::process::Command;
+
+#[test]
+fn file_not_found() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("pixelmosh")?;
+
+    cmd.arg("test/file/not/found");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("File not found"));
+
+    Ok(())
+}
+
+#[test]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("pixelmosh")?;
+
+    cmd.arg("src/tests/test.png")
+       .arg("--min-rate")
+       .arg("5")
+       .arg("--max-rate")
+       .arg("10")
+       .arg("--line-shift")
+       .arg("0.2")
+       .arg("--seed")
+       .arg("1309999")
+       .assert()
+       .success();
+
+    Ok(())
+}
