2090e27f06dc41411f0544700087e7bc52551ab8
-k <slowdive@me.com>
2022-07-08 01:38:19 -0700

    Fix `read_file`

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,7 @@ struct Args {
     seed: u64,
 }
 
-fn read_file(file: String) -> std::io::Result<(std::vec::Vec<u8>, png::OutputInfo)> {
+fn read_file(file: String) -> (std::vec::Vec<u8>, png::OutputInfo) {
     let input = File::open(file);
     let input = match input {
         Ok(file) => file,
@@ -52,7 +52,7 @@ fn read_file(file: String) -> std::io::Result<(std::vec::Vec<u8>, png::OutputInf
     let mut reader = decoder.read_info().unwrap();
     let mut buf = vec![0; reader.output_buffer_size()];
     let info = reader.next_frame(&mut buf).unwrap();
-    Ok((buf, info))
+    (buf, info)
 }
 
 fn write_file(buf: &[u8], info: &png::OutputInfo) {
@@ -105,7 +105,7 @@ fn main() {
 
     spinner.set_message("Reading input");
     let mut rng = ChaCha8Rng::seed_from_u64(seed);
-    let (mut buf, info) = read_file(args.file).unwrap();
+    let (mut buf, info) = read_file(args.file);
 
     spinner.set_message("\x1b[94mProcessing\x1b[0m");
     engine::mosh(&info, &mut buf, &mut rng, &options);
