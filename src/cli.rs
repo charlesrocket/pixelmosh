4cc62ec26dd1f23642cb20bbdd571790c7ab7d71
-k <slowdive@me.com>
2022-07-11 19:23:06 -0700

    Unwrap `cli` functions

--- src/cli.rs
+++ src/cli.rs
@@ -30,19 +30,43 @@ pub fn read_file(file: String) -> (std::vec::Vec<u8>, png::OutputInfo) {
     };
 
     let mut buf = vec![0; reader.output_buffer_size()];
-    let info = reader.next_frame(&mut buf).unwrap();
+    let info = match reader.next_frame(&mut buf) {
+        Ok(info) => info,
+        Err(_) => std::process::exit(1),
+    };
+
     (buf, info)
 }
 
 pub fn write_file(path: &str, buf: &[u8], info: &png::OutputInfo) {
     let path = Path::new(&path);
-    let output = File::create(path).unwrap();
+    let output = match File::create(path) {
+        Ok(output) => output,
+        Err(error) => {
+            eprintln!("\x1b[1;31mpath error:\x1b[0m {}", error);
+            std::process::exit(1)
+        }
+    };
+
     let buf_writer = &mut BufWriter::new(output);
     let mut encoder = png::Encoder::new(buf_writer, info.width, info.height);
 
     encoder.set_color(info.color_type);
     encoder.set_depth(info.bit_depth);
 
-    let mut writer = encoder.write_header().unwrap();
-    writer.write_image_data(buf).unwrap();
+    let mut writer = match encoder.write_header() {
+        Ok(writer) => writer,
+        Err(error) => {
+            eprintln!("\x1b[1;31mwrite error:\x1b[0m {}", error);
+            std::process::exit(1)
+        }
+    };
+
+    match writer.write_image_data(buf) {
+        Ok(buf) => buf,
+        Err(error) => {
+            eprintln!("{}", error);
+            std::process::exit(1)
+        }
+    };
 }
