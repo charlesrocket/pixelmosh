626ba1035164b864ede09fecb37b38472da8f817
-k <slowdive@me.com>
2022-07-13 20:43:58 -0700

    Unwrap `read_file`

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -21,7 +21,7 @@ fn engine() {
     };
 
     let mut rng = ChaCha8Rng::seed_from_u64(901042006);
-    let (mut buf, info) = cli::read_file("src/utils/test.png".to_string());
+    let (mut buf, info) = cli::read_file("src/utils/test.png".to_string()).unwrap();
 
     engine::mosh(&info, &mut buf, &mut rng, &options);
     cli::write_file(&"moshed.png".to_string(), &buf, &info);
