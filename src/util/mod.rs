371e6c7f39318ffe90cad7286ec2a46c8683d499
-k <slowdive@me.com>
2022-07-22 21:43:14 -0700

    Fix `mosh`

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -19,12 +19,13 @@ fn mosh() {
         flip_rng,
         channel_swap_rng,
         channel_shift_rng,
+        pixelation,
     };
 
     let mut rng = ChaCha8Rng::seed_from_u64(901_042_006);
     let (mut buf, info) = cli::read_file("src/util/test.png".to_string()).unwrap();
 
-    let image = libmosh::mosh(&info, &mut buf, pixelation, &mut rng, &options).unwrap();
+    let image = libmosh::mosh(&info, &mut buf, &mut rng, &options).unwrap();
     cli::write_file("moshed.png", &image, &info).unwrap();
 
     let output = std::fs::File::open("moshed.png").unwrap();
