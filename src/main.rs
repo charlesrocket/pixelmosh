3b7ae773aca81593fdb296ca6988016708fab4ad
-k <slowdive@me.com>
2022-07-30 18:35:44 -0700

    Use default options

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use indicatif::{ProgressBar, ProgressStyle};
 use rand::prelude::*;
 use rand_chacha::ChaCha8Rng;
 
-use libmosh::cli;
+use libmosh::{cli, Options};
 
 #[derive(Parser, Debug)]
 #[clap(version, author = cli::BANNER, about, long_about = None)]
@@ -11,28 +11,28 @@ struct Args {
     #[clap(required = true, display_order = 1)]
     file: String,
 
-    #[clap(short, long, default_value_t = 1, display_order = 2)]
+    #[clap(short, long, default_value_t = Options::default().min_rate, display_order = 2)]
     min_rate: u16,
 
-    #[clap(short = 'n', long, default_value_t = 7, display_order = 3)]
+    #[clap(short = 'n', long, default_value_t = Options::default().max_rate, display_order = 3)]
     max_rate: u16,
 
-    #[clap(short, long, default_value_t = 0.3, display_order = 4)]
+    #[clap(short, long, default_value_t = Options::default().line_shift_rng, display_order = 4)]
     line_shift: f64,
 
-    #[clap(short, long, default_value_t = 0.3, display_order = 5)]
+    #[clap(short, long, default_value_t = Options::default().reverse_rng, display_order = 5)]
     reverse: f64,
 
-    #[clap(short, long, default_value_t = 0.3, display_order = 6)]
+    #[clap(short, long, default_value_t = Options::default().flip_rng, display_order = 6)]
     flip: f64,
 
-    #[clap(short, long, default_value_t = 0.3, display_order = 7)]
+    #[clap(short, long, default_value_t = Options::default().channel_swap_rng, display_order = 7)]
     channel_swap: f64,
 
-    #[clap(short = 't', long, default_value_t = 0.3, display_order = 8)]
+    #[clap(short = 't', long, default_value_t = Options::default().channel_shift_rng, display_order = 8)]
     channel_shift: f64,
 
-    #[clap(short, long, default_value_t = 10, display_order = 9)]
+    #[clap(short, long, default_value_t = Options::default().pixelation, display_order = 9)]
     pixelation: u8,
 
     #[clap(short, long, default_value_t = thread_rng().next_u64(),
