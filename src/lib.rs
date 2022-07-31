322d1781d5628dc731b1895aef9a94a25fb59c03
-k <slowdive@me.com>
2022-07-30 18:22:36 -0700

    Move implementations

--- src/lib.rs
+++ src/lib.rs
@@ -47,6 +47,55 @@ enum MoshLine {
     Reverse,
 }
 
+impl Mosh for MoshChunk {
+    fn run(&self, chunk: &mut [u8]) {
+        match self {
+            Self::ChannelSwap(channel_1, channel_2, channel_count) => {
+                let chunk_length = chunk.len();
+                let channel_value_count = chunk_length / channel_count;
+
+                for i in 0..channel_value_count {
+                    let channel_1_index = (i * channel_count) + channel_1;
+                    let channel_2_index = (i * channel_count) + channel_2;
+                    let channel_1_value = chunk[channel_1_index];
+                    let channel_2_value = chunk[channel_2_index];
+
+                    chunk[channel_1_index] = channel_2_value;
+                    chunk[channel_2_index] = channel_1_value;
+                }
+            }
+
+            Self::Flip => {
+                chunk.reverse();
+            }
+        }
+    }
+}
+
+impl Mosh for MoshLine {
+    fn run(&self, line: &mut [u8]) {
+        match self {
+            Self::ChannelShift(amount, channel, channel_count) => {
+                let line_length = line.len();
+                let channel_value_count = line_length / channel_count;
+
+                for i in 0..channel_value_count {
+                    line[(i * channel_count + channel) % line_length] =
+                        line[(i * channel_count + channel + (channel + 1) * amount) % line_length];
+                }
+            }
+
+            Self::Shift(amount) => {
+                line.rotate_left(*amount);
+            }
+
+            Self::Reverse => {
+                line.reverse();
+            }
+        }
+    }
+}
+
 /// Processes provided image data.
 ///
 /// # Errors
@@ -239,55 +288,6 @@ fn chunkmosh(
     if flip {
         MoshChunk::Flip.run(chunk);
     };
-}
-
-impl Mosh for MoshChunk {
-    fn run(&self, chunk: &mut [u8]) {
-        match self {
-            Self::ChannelSwap(channel_1, channel_2, channel_count) => {
-                let chunk_length = chunk.len();
-                let channel_value_count = chunk_length / channel_count;
-
-                for i in 0..channel_value_count {
-                    let channel_1_index = (i * channel_count) + channel_1;
-                    let channel_2_index = (i * channel_count) + channel_2;
-                    let channel_1_value = chunk[channel_1_index];
-                    let channel_2_value = chunk[channel_2_index];
-
-                    chunk[channel_1_index] = channel_2_value;
-                    chunk[channel_2_index] = channel_1_value;
-                }
-            }
-
-            Self::Flip => {
-                chunk.reverse();
-            }
-        }
-    }
-}
-
-impl Mosh for MoshLine {
-    fn run(&self, line: &mut [u8]) {
-        match self {
-            Self::ChannelShift(amount, channel, channel_count) => {
-                let line_length = line.len();
-                let channel_value_count = line_length / channel_count;
-
-                for i in 0..channel_value_count {
-                    line[(i * channel_count + channel) % line_length] =
-                        line[(i * channel_count + channel + (channel + 1) * amount) % line_length];
-                }
-            }
-
-            Self::Shift(amount) => {
-                line.rotate_left(*amount);
-            }
-
-            Self::Reverse => {
-                line.reverse();
-            }
-        }
-    }
 }
 
 #[cfg(test)]
