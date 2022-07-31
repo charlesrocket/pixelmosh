ba1f64cb791c0e125db637dd49259dfe69c1f486
-k <slowdive@me.com>
2022-07-30 18:45:03 -0700

    Use unittests

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -6,102 +6,45 @@ use super::*;
 
 #[test]
 fn rgb() {
-    let min_rate = 5;
-    let max_rate = 7;
-    let line_shift_rng = 0.8;
-    let reverse_rng = 0.4;
-    let flip_rng = 0.3;
-    let channel_swap_rng = 0.9;
-    let channel_shift_rng = 0.5;
-    let pixelation = 10;
-    let options = Options {
-        min_rate,
-        max_rate,
-        line_shift_rng,
-        reverse_rng,
-        flip_rng,
-        channel_swap_rng,
-        channel_shift_rng,
-        pixelation,
-    };
-
     let mut rng = ChaCha8Rng::seed_from_u64(901_042_006);
     let (mut buf, info) = cli::read_file("src/util/test-rgb.png".to_string()).unwrap();
 
-    mosh(&info, &mut buf, &mut rng, &options).unwrap();
+    mosh(&info, &mut buf, &mut rng, &Options::default()).unwrap();
     cli::write_file("moshed.png", &buf, &info).unwrap();
 
     let output = std::fs::File::open("moshed.png").unwrap();
     let mut file = std::io::BufReader::new(output);
     let checksum = adler32(&mut file).unwrap();
 
-    assert_eq!(checksum, 2_761_705_494);
+    assert_eq!(checksum, 2_675_922_770);
 }
 
 #[test]
 fn rgba() {
-    let min_rate = 5;
-    let max_rate = 7;
-    let line_shift_rng = 0.8;
-    let reverse_rng = 0.4;
-    let flip_rng = 0.3;
-    let channel_swap_rng = 0.9;
-    let channel_shift_rng = 0.5;
-    let pixelation = 10;
-    let options = Options {
-        min_rate,
-        max_rate,
-        line_shift_rng,
-        reverse_rng,
-        flip_rng,
-        channel_swap_rng,
-        channel_shift_rng,
-        pixelation,
-    };
-
     let mut rng = ChaCha8Rng::seed_from_u64(901_042_006);
     let (mut buf, info) = cli::read_file("src/util/test-rgb-alpha.png".to_string()).unwrap();
 
-    mosh(&info, &mut buf, &mut rng, &options).unwrap();
+    mosh(&info, &mut buf, &mut rng, &Options::default()).unwrap();
     cli::write_file("moshed.png", &buf, &info).unwrap();
 
     let output = std::fs::File::open("moshed.png").unwrap();
     let mut file = std::io::BufReader::new(output);
     let checksum = adler32(&mut file).unwrap();
 
-    assert_eq!(checksum, 1_601_040_326);
+    assert_eq!(checksum, 3_414_819_387);
 }
 
 #[test]
 fn grayscale() {
-    let min_rate = 5;
-    let max_rate = 7;
-    let line_shift_rng = 0.8;
-    let reverse_rng = 0.4;
-    let flip_rng = 0.3;
-    let channel_swap_rng = 0.9;
-    let channel_shift_rng = 0.5;
-    let pixelation = 10;
-    let options = Options {
-        min_rate,
-        max_rate,
-        line_shift_rng,
-        reverse_rng,
-        flip_rng,
-        channel_swap_rng,
-        channel_shift_rng,
-        pixelation,
-    };
-
     let mut rng = ChaCha8Rng::seed_from_u64(901_042_006);
     let (mut buf, info) = cli::read_file("src/util/test-grayscale.png".to_string()).unwrap();
 
-    mosh(&info, &mut buf, &mut rng, &options).unwrap();
+    mosh(&info, &mut buf, &mut rng, &Options::default()).unwrap();
     cli::write_file("moshed.png", &buf, &info).unwrap();
 
     let output = std::fs::File::open("moshed.png").unwrap();
     let mut file = std::io::BufReader::new(output);
     let checksum = adler32(&mut file).unwrap();
 
-    assert_eq!(checksum, 220_441_887);
+    assert_eq!(checksum, 376_952_495);
 }
