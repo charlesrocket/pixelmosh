6a916a50c5a9e75f090b16916e401ed7e62bb800
-k <slowdive@me.com>
2022-07-31 01:21:33 -0700

    Add `fx` module

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,10 @@ use resize::Pixel::{Gray8, RGB8, RGBA8};
 use resize::Type::Point;
 use rgb::FromSlice;
 
+use crate::fx::{Mosh, MoshChunk, MoshLine};
+
 pub mod cli;
+pub mod fx;
 pub mod ops;
 
 /// Processing options
@@ -44,70 +47,6 @@ impl Default for Options {
             channel_swap_rng: 0.3,
             channel_shift_rng: 0.3,
             pixelation: 10,
-        }
-    }
-}
-
-trait Mosh {
-    fn run(&self, chunk: &mut [u8]);
-}
-
-enum MoshChunk {
-    ChannelSwap(usize, usize, usize),
-    Flip,
-}
-
-enum MoshLine {
-    ChannelShift(usize, usize, usize),
-    Shift(usize),
-    Reverse,
-}
-
-impl Mosh for MoshChunk {
-    fn run(&self, chunk: &mut [u8]) {
-        match self {
-            Self::ChannelSwap(channel_1, channel_2, channel_count) => {
-                let chunk_length = chunk.len();
-                let channel_value_count = chunk_length / channel_count;
-
-                for i in 0..channel_value_count {
-                    let channel_1_index = (i * channel_count) + channel_1;
-                    let channel_2_index = (i * channel_count) + channel_2;
-                    let channel_1_value = chunk[channel_1_index];
-                    let channel_2_value = chunk[channel_2_index];
-
-                    chunk[channel_1_index] = channel_2_value;
-                    chunk[channel_2_index] = channel_1_value;
-                }
-            }
-
-            Self::Flip => {
-                chunk.reverse();
-            }
-        }
-    }
-}
-
-impl Mosh for MoshLine {
-    fn run(&self, line: &mut [u8]) {
-        match self {
-            Self::ChannelShift(amount, channel, channel_count) => {
-                let line_length = line.len();
-                let channel_value_count = line_length / channel_count;
-
-                for i in 0..channel_value_count {
-                    line[(i * channel_count + channel) % line_length] =
-                        line[(i * channel_count + channel + (channel + 1) * amount) % line_length];
-                }
-            }
-
-            Self::Shift(amount) => {
-                line.rotate_left(*amount);
-            }
-
-            Self::Reverse => {
-                line.reverse();
-            }
         }
     }
 }
