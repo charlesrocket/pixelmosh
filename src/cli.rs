53645feda30762759780f1eb558db7adf95e33e2
-k <slowdive@me.com>
2022-08-11 18:49:49 -0700

    Drop TODO

--- src/cli.rs
+++ src/cli.rs
@@ -33,7 +33,5 @@ pub const SPINNER_2: [&str; 7] = [
 /// Returns `true` if a terminal emulator is detected.
 #[must_use]
 pub fn display_var() -> bool {
-    // TODO
-    // $TERM?
     matches!(env::var("DISPLAY"), Ok(_))
 }
