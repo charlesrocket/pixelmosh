5596dd140fe2370ed6752a9aec91e092c97cc774
-k <slowdive@me.com>
2023-01-07 23:31:18 -0800

    Add CSS

--- src/gui.rs
+++ src/gui.rs
@@ -1,5 +1,8 @@
 use adw::gio;
 use adw::prelude::*;
+use gtk::gdk::Display;
+use gtk::{CssProvider, StyleContext};
+
 use window::Window;
 
 mod image;
@@ -11,6 +14,7 @@ pub fn start() {
     gio::resources_register_include!("pixelmosh.gresource").expect("Failed to register resources.");
     let app = adw::Application::builder().application_id(APP_ID).build();
 
+    app.connect_startup(|_| load_css());
     app.connect_activate(build_ui);
     app.run();
 }
@@ -21,4 +25,15 @@ fn build_ui(app: &adw::Application) {
 
     style_manager.set_color_scheme(adw::ColorScheme::ForceDark);
     window.present();
+}
+
+fn load_css() {
+    let provider = CssProvider::new();
+    provider.load_from_data(include_bytes!("../resources/style.css"));
+
+    StyleContext::add_provider_for_display(
+        &Display::default().expect("Could not connect to a display."),
+        &provider,
+        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+    );
 }
