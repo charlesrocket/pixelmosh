36d9fa8eaa0d59c76e9380ae95840faba0beafeb
-k <slowdive@me.com>
2023-01-10 15:08:26 -0800

    Move resources

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 fn main() {
     #[cfg(feature = "gui")]
     glib_build_tools::compile_resources(
-        "resources",
-        "resources/resources.gresource.xml",
+        "src/resources",
+        "src/resources/resources.gresource.xml",
         "pixelmosh.gresource",
     );
 }
