fe66ac7768ec5fe4f525333d2c32571a89b2b9ef
-k <slowdive@me.com>
2023-01-11 16:12:30 -0800

    Fix `Image`

--- src/gui/image.rs
+++ src/gui/image.rs
@@ -1,20 +1,17 @@
 use glib::Cast;
 use gtk::{gdk, glib};
-use png::{BitDepth, ColorType, OutputInfo};
+use png::ColorType;
 
 use std::path::{Path, PathBuf};
 
 use libmosh::{
     err::MoshError,
-    mosh,
     ops::{read_file, write_file},
-    MoshOptions,
+    MoshData, MoshOptions,
 };
 
 pub struct Image {
-    pub buf: Vec<u8>,
-    pub buf_new: Vec<u8>,
-    pub info: OutputInfo,
+    pub data: MoshData,
     pub texture: gdk::Texture,
 }
 
@@ -24,16 +21,7 @@ pub struct Options(MoshOptions);
 impl Image {
     fn new() -> Self {
         Self {
-            buf: vec![0_u8],
-            buf_new: vec![0_u8],
-            info: OutputInfo {
-                width: 1,
-                height: 1,
-                color_type: ColorType::Rgb,
-                bit_depth: BitDepth::Eight,
-                line_size: 1,
-            },
-
+            data: MoshData::default(),
             texture: gdk::MemoryTexture::new(
                 1_i32,
                 1_i32,
@@ -45,37 +33,43 @@ impl Image {
         }
     }
 
-    fn generate_texture(info: &OutputInfo, buf: &Vec<u8>) -> gdk::MemoryTexture {
-        let (format, stride) = match &info.color_type {
+    fn generate_texture(
+        buf: &Vec<u8>,
+        width: u32,
+        height: u32,
+        color_type: ColorType,
+        line_size: usize,
+    ) -> gdk::MemoryTexture {
+        let (format, stride) = match &color_type {
             ColorType::GrayscaleAlpha | ColorType::Indexed => {
                 todo!()
             }
 
             ColorType::Grayscale => {
                 let mut rgb = Vec::with_capacity(buf.len());
-                for row in buf.chunks_exact(info.line_size) {
-                    for &pixel in &row[..info.width as usize] {
+                for row in buf.chunks_exact(line_size) {
+                    for &pixel in &row[..width as usize] {
                         rgb.extend_from_slice(&[pixel, pixel, pixel]);
                     }
                 }
 
                 return gdk::MemoryTexture::new(
-                    info.width as i32,
-                    info.height as i32,
+                    width as i32,
+                    height as i32,
                     gdk::MemoryFormat::R8g8b8,
                     &glib::Bytes::from_owned(rgb),
-                    info.width as usize * 3,
+                    width as usize * 3,
                 )
                 .upcast();
             }
 
-            ColorType::Rgb => (gdk::MemoryFormat::R8g8b8, (info.width * 3)),
-            ColorType::Rgba => (gdk::MemoryFormat::R8g8b8a8, (info.width * 4)),
+            ColorType::Rgb => (gdk::MemoryFormat::R8g8b8, (width * 3)),
+            ColorType::Rgba => (gdk::MemoryFormat::R8g8b8a8, (width * 4)),
         };
 
         gdk::MemoryTexture::new(
-            info.width as i32,
-            info.height as i32,
+            width as i32,
+            height as i32,
             format,
             &glib::Bytes::from(buf),
             stride as usize,
@@ -83,29 +77,46 @@ impl Image {
     }
 
     pub fn open_file(&mut self, file: &PathBuf) -> Result<(), MoshError> {
-        let (buf, info) = read_file(file)?;
-        let texture = Self::generate_texture(&info, &buf);
+        let input = read_file(file)?;
 
-        self.buf = buf;
-        self.info = info;
+        self.data = MoshData::new(&input).unwrap();
+
+        let texture = Self::generate_texture(
+            &self.data.buf,
+            self.data.width,
+            self.data.height,
+            self.data.color_type,
+            self.data.line_size,
+        );
+
         self.texture = texture.upcast();
 
         Ok(())
     }
 
     pub fn save_file(&mut self, file: &Path) -> Result<(), MoshError> {
-        write_file(file.to_str().unwrap(), &self.buf_new, &self.info)?;
+        write_file(
+            file.to_str().unwrap(),
+            &self.data.buf,
+            self.data.width,
+            self.data.height,
+            self.data.color_type,
+            self.data.bit_depth,
+        )?;
 
         Ok(())
     }
 
     pub fn mosh(&mut self, options: &Options) {
-        self.buf_new.clear();
-        let mut buf = self.buf.clone();
-        mosh(&self.info, &mut buf, &options.0).unwrap();
-
-        self.texture = Self::generate_texture(&self.info, &buf).upcast();
-        self.buf_new = buf;
+        self.data.mosh(&options.0).unwrap();
+        self.texture = Self::generate_texture(
+            &self.data.buf,
+            self.data.width,
+            self.data.height,
+            self.data.color_type,
+            self.data.line_size,
+        )
+        .upcast();
     }
 
     pub fn get_texture(&mut self) -> gdk::Texture {
