6a3bda316d4ade9c0fee33e06a2b848692d26f54
-k <slowdive@me.com>
2023-01-26 14:28:27 -0800

    Update benchmarks

--- benches/mosh.rs
+++ benches/mosh.rs
@@ -1,9 +1,7 @@
 #![feature(test)]
 extern crate test;
 
-use test::black_box;
-
-use libmosh::{MoshData, MoshOptions};
+use libmosh::MoshCore;
 
 use crate::images::{GRAYSCALE, RGB, RGBA};
 
@@ -12,53 +10,53 @@ mod images;
 struct Bench {}
 
 impl Bench {
-    fn bench_image(input: u64, image: &[u8]) {
-        let options = MoshOptions {
-            min_rate: 3,
-            max_rate: 3,
-            pixelation: 2,
-            line_shift: 1.0,
-            reverse: 1.0,
-            flip: 1.0,
-            channel_swap: 1.0,
-            channel_shift: 1.0,
-            seed: input,
-        };
+    fn bench_image(image: &[u8]) {
+        let mut core = MoshCore::new();
 
-        let mut image = MoshData::new(image).unwrap();
+        core.read_image(image).unwrap();
 
-        image.mosh(&options).unwrap();
+        core.options.min_rate = 3;
+        core.options.max_rate = 3;
+        core.options.pixelation = 2;
+        core.options.line_shift = 1.0;
+        core.options.reverse = 1.0;
+        core.options.flip = 1.0;
+        core.options.channel_swap = 1.0;
+        core.options.channel_shift = 1.0;
+        core.options.seed = 42;
+
+        core.mosh().unwrap();
     }
 }
 
-fn rgb(value: u64) {
-    Bench::bench_image(value, RGB);
+fn rgb() {
+    Bench::bench_image(RGB);
 }
 
-fn rgba(value: u64) {
-    Bench::bench_image(value, RGBA);
+fn rgba() {
+    Bench::bench_image(RGBA);
 }
 
-fn grayscale(value: u64) {
-    Bench::bench_image(value, GRAYSCALE);
+fn grayscale() {
+    Bench::bench_image(GRAYSCALE);
 }
 
 #[cfg(test)]
 mod benches {
-    use super::{black_box, grayscale, rgb, rgba, test};
+    use super::{grayscale, rgb, rgba, test};
 
     #[bench]
     fn bench_rgb(b: &mut test::Bencher) {
-        b.iter(|| rgb(black_box(20)));
+        b.iter(|| rgb());
     }
 
     #[bench]
     fn bench_rgba(b: &mut test::Bencher) {
-        b.iter(|| rgba(black_box(20)));
+        b.iter(|| rgba());
     }
 
     #[bench]
     fn bench_grayscale(b: &mut test::Bencher) {
-        b.iter(|| grayscale(black_box(20)));
+        b.iter(|| grayscale());
     }
 }
