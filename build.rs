cabe8ed68afdb48b292102d20c6ea5b9a77c639b
-k <slowdive@me.com>
2023-03-04 05:45:05 -0800

    Fix build script

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@
 fn main() {
     #[cfg(feature = "gui")]
     glib_build_tools::compile_resources(
-        "src/resources",
+        &["src/resources"],
         "src/resources/resources.gresource.xml",
         "pixelmosh.gresource",
     );
