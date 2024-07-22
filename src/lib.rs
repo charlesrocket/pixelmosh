f933614ca4ab615765cd44b53daba40850dc89ed
-k <slowdive@me.com>
2024-07-21 23:24:56 -0700

    fix: use `clone_from()`

--- src/lib.rs
+++ src/lib.rs
@@ -135,7 +135,7 @@ impl MoshCore {
         let mut buf = vec![0_u8; reader.output_buffer_size()];
         let info = reader.next_frame(&mut buf)?;
 
-        self.data.buf = buf.clone();
+        self.data.buf.clone_from(&buf);
         self.data.image = buf;
         self.data.width = info.width;
         self.data.height = info.height;
