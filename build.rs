99451ab61056273ae45ec1570033f51e2f54690e
-k <slowdive@me.com>
2025-03-13 22:48:14 -0400

    build(script): add gui block

--- build.rs
+++ build.rs
@@ -2,9 +2,11 @@
 
 fn main() {
     #[cfg(feature = "gui")]
-    glib_build_tools::compile_resources(
-        &["src/resources"],
-        "src/resources/resources.gresource.xml",
-        "pixelmosh.gresource",
-    );
+    {
+        glib_build_tools::compile_resources(
+            &["src/resources"],
+            "src/resources/resources.gresource.xml",
+            "pixelmosh.gresource",
+        );
+    }
 }
